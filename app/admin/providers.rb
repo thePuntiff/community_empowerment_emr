84f6f4810686828269d030092c3f85c26a39e648
Raghu Betina <raghu@firstdraft.com>
2018-11-29 03:10:47 +0000

    Whitelist columns in Provider for ActiveAdmin

--- app/admin/providers.rb
+++ app/admin/providers.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Provider do
+
+ permit_params :firstname, :lastname, :email, :password
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
