e8d975367bada83bd66de4d50dc3e0c633ed5a0a
Raghu Betina <raghu@firstdraft.com>
2018-11-29 03:11:09 +0000

    Whitelist columns in Encounter for ActiveAdmin

--- app/admin/encounters.rb
+++ app/admin/encounters.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Encounter do
+
+ permit_params :provider_id, :patient_id, :chief_complaint, :notes, :location_id, :approving_provider_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
