0da47f4a43b97fa72839898855093d2c56914a9c
Raghu Betina <raghu@firstdraft.com>
2018-11-29 03:10:41 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "patients#index"
   # Routes for the Sex resource:
 
