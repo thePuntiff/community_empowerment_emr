e170123b0e390f7708905c9396a08b2232684836
Raghu Betina <raghu@firstdraft.com>
2018-11-29 03:11:04 +0000

    Whitelist columns in ProviderCredential for ActiveAdmin

--- app/admin/provider_credentials.rb
+++ app/admin/provider_credentials.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register ProviderCredential do
+
+ permit_params :provider_id, :credential_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
