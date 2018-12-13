bfb10571c843fd9db50562384acb232b77eeedb4
Zak Randall <zrandall@chicagobooth.edu>
2018-12-13 15:46:56 +0000

    final
    attempt at chartkick, though leaving out for final submission

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -1,6 +1,15 @@
 class AdminPanelController < ApplicationController
 
   def index
+    @prescription_names = Prescription.all.name
+    @sex_names = []
+    Patient.all.each do |patient|
+      if patient.sex_id == 1
+        @sex_names.push("Male")
+      else
+        @sex_names.push("Female")
+      end
+    end
     render("admin_templates/index.html.erb")
   end
   
